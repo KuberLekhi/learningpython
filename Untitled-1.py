41a653775c28944c1f645aa2ced228c6a9a34f87
Kuber Lekhi <155307227+KuberLekhi@users.noreply.github.com>
2024-10-19 04:42:54 +0530

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,23 @@
+import time 
+name = input ("what is your name ?\n")
+menu = "\n-tea\n-coffee\n-juice\n-milk"
+print ("Hello " + name + ", I hope you're doing good. What would you like to have from our menu?" + menu )
+order = input()
+print ("Great choice " + name + ", How many " + order + "'s would you like?") 
+quantity = int(input())
+print ("Okay " + name + ", Your order will arrive shortly")
+if order == 'tea' : 
+   price = 50
+elif order == 'milk': 
+   price = 70 
+elif order == 'juice': 
+   price = 90
+elif order == 'coffee':
+   price = 100
+else :
+   price = 200
+time.sleep(3)
+print("How was your " + order + " " + name + "?" )
+response = input()
+bill = price*quantity
+print("I'm glad you found your " + order + " " + response + "\nYour total will be " + str(bill) + "rs" )
